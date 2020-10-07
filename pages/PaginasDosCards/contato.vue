66aca41cd2eb74da1f00a4075635909e95abe1c9
ninofatec <rtamaio.jr@gmail.com>
2020-10-07 13:56:16 -0300

    Update contato.vue

--- pages/PaginasDosCards/contato.vue
+++ pages/PaginasDosCards/contato.vue
@@ -5,32 +5,32 @@
     <br>
     <br>
     <p><b>Renato Tamaio</b></p>
-    <p>Telefone (16) 33333333</p>
-    <p>Celular (16) 999999999</p>
+    <p>Telefone: (16) 33333333</p>
+    <p>Celular: (16) 999999999</p>
     <p><a href="https://github.com/renatotamaio/">https://github.com/renatotamaio/</a></p>
     <p><a href="https://www.facebook.com/renato.tamaio">Facebook</a></p>
     <p><a href="https://www.instagram.com/renatotamaio/">Instagram</a></p>
     <br>
     <br>
     <p><b>Antonio Gambogi</b></p>
-        <p>Telefone (16) 33333333</p>
-    <p>Celular (16) 999999999</p>
+        <p>Telefone: (16) 33333333</p>
+    <p>Celular: (16) 999999999</p>
     <p><a href="https://github.com/antoniogambogi/">https://github.com/antoniogambogi/</a></p>
     <p><a href="https://www.facebook.com/antonio.gambogi">Facebook</a></p>
     <p><a href="https://www.instagram.com/antoniogambogi/">Instagram</a></p>
     <br>
     <br>
     <p><b>Antonio Ezarqui</b></p>
-        <p>Telefone (16) 33333333</p>
-    <p>Celular (16) 999999999</p>
+        <p>Telefone: (16) 33333333</p>
+    <p>Celular: (16) 999999999</p>
     <p><a href="https://github.com/antonioezarqui/">https://github.com/antonioezarqui/</a></p>
     <p><a href="https://www.facebook.com/antoniocarlos.ezarquijunior">Facebook</a></p>
     <p><a href="https://www.instagram.com/antonioezarqui/">Instagram</a></p>
     <br>
     <br>
     <p><b>Bruno Silva</b></p>
-        <p>Telefone (16) 33333333</p>
-    <p>Celular (16) 999999999</p>
+        <p>Telefone: (16) 33333333</p>
+    <p>Celular: (16) 999999999</p>
     <p><a href="...">Github</a></p>
     <p><a href="https://www.facebook.com/profile.php?id=100002509667688">Facebook</a></p>
     <p><a href="...">Instagram</a></p>
